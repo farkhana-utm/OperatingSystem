2760f1b68f50eb2c2151dfdfd11e60c02727ff14
Mahyuddin Husairi <mahyuddin@gmail.com>
2023-12-20 07:21:33 +0800

    new lab exercise
    Lab 4 exercise - Linux Process Management and Monitoring

---
+++ Lab4/child2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void handle_signal(int sig) {
+    printf("Child2 process (ID: %d) received signal: %d. Terminating...\n", getpid(), sig);
+    exit(0);
+}
+
+int main() {
+    signal(SIGTERM, handle_signal);
+    signal(SIGINT, handle_signal);
+
+    printf("I am child2 with process ID: %d and parent process ID: %d\n", getpid(), getppid());
+    sleep(1800); // Sleep for 30 minutes
+    return 0;
+}
